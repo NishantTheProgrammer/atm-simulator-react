97cf17d8f533e2c158a98a375db4b6abfce44d39
NishantTheProgrammer <knishant113@gmail.com>
2026-01-19 18:40:10 +0530

    added appy card modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
+import ReactModal from "react-modal"
 import Machine from "./components/Machine"
+import ApplyCard from "./components/ApplyCard"
+import { useState } from "react"
 
 
 function App() {
+
+  const [showApplyModal, setShowApplyModal] = useState(false);
 
 
 
   return (
     <>
       <Machine />
+      <button onClick={() => setShowApplyModal(true)} >Appy for card</button>
+      <ReactModal 
+        isOpen={showApplyModal} 
+        onRequestClose={() => setShowApplyModal(false)} 
+        >
+        <ApplyCard />
+      </ReactModal>
     </>
   )
 }
